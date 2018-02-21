aaafd54754630a82845bafff69d58b041563d93f
sbt-arshinkin-da <DAArshinkin.SBT@sberbank.ru>
2018-02-21 17:30:05 +0300

    Added WebSockets

--- src/main/java/ru/sbertech/coursejava/Main.java
+++ src/main/java/ru/sbertech/coursejava/Main.java
@@ -1,10 +1,14 @@
 package ru.sbertech.coursejava;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import ru.sbertech.coursejava.accounts.AccountService;
 import ru.sbertech.coursejava.accounts.AcountServiceDBImpl;
+import ru.sbertech.coursejava.chat.WebSocketChatServlet;
 import ru.sbertech.coursejava.dbService.DBH2Service;
 import ru.sbertech.coursejava.dbService.DBService;
 import ru.sbertech.coursejava.servlets.MirrorRequestServlet;
@@ -24,9 +28,17 @@ public class Main {
         context.addServlet(new ServletHolder(new MirrorRequestServlet()), "/mirror");
         context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
         context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
 
         Server server = new Server(8080);
-        server.setHandler(context);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
 
         server.start();
         System.out.println("Server started");
